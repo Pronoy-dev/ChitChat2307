be5ca75c5c215b25600622ace63f9ad0edd7fda3
Pronoy-dev <propronoy3@gmail.com>
2024-10-05 15:00:48 +0600

    Redux setup start

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import dbapp from "./FirebaseConfiguration/FirebaseDbConnection.js";
+import { store } from "./Features/Redux/store/Store.js";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
